4fd7b51ca70c52d6f0155792d61c2387e4a2f3ee
ethanlixian <ethanlixian@didichuxing.com>
2020-10-15 11:33:02 +0800

    依赖

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,8 @@
 module.exports = (api, options) => {
+    api.extendPackage({
+        dependencies: {
+            "@vue/cli-service": "^4.5.7"
+        },
+    });
     api.render('./template', options);
 };
-  
